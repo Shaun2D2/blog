c3c7310522155ec5963115fa1a511546113a25ce
Shaun Lobisnger <Shaun.lobsinger@gmail.com>
2018-12-17 23:02:16 -0700

    wip

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 const config = {
     base: 'https://www.googleapis.com/blogger/v3/blogs/3614409420169360129',
-    key: 'AIzaSyCnH3Y0ASsl19SMVv4XHodmqDEzR7GgJAg'
+    key: ''
 }
 
 export default config;
